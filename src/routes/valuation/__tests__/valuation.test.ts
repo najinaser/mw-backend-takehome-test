cd2fa90ac1c75f05e215ebcfd6122b5d2271a836
najinaser <52705021+najinaser@users.noreply.github.com>
2025-07-20 13:44:21 +0100

    make current put test pass and built test for get valuation

--- src/routes/valuation/__tests__/valuation.test.ts
+++ src/routes/valuation/__tests__/valuation.test.ts
@@ -1,7 +1,72 @@
+import { vi } from 'vitest';
+vi.mock('@app/super-car/super-car-valuation', () => ({
+  fetchValuationFromSuperCarValuation: vi.fn(() => ({
+    vrm: 'ABC123',
+    lowestValue: 5000,
+    highestValue: 8000,
+  })),
+}));
+
 import { fastify } from '~root/test/fastify';
 import { VehicleValuationRequest } from '../types/vehicle-valuation-request';
+import { VehicleValuation } from '@app/models/vehicle-valuation';
 
 describe('ValuationController (e2e)', () => {
+
+  beforeEach(async () => {
+    const repo = fastify.orm.getRepository(VehicleValuation);
+    await repo.clear();
+  });
+
+  describe('GET /valuations/', () => {
+    it("shoudl return 400 if VRM is more than 7 characters", async () => {
+      const res = await fastify.inject({
+        url: '/valuations/12345678',
+        method: 'GET',
+      });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.json()).toEqual({
+        message: 'vrm must be 7 characters or less',
+        statusCode: 400,
+      });
+    })
+    it('should return 404 if valuation not found', async () => {
+      const res = await fastify.inject({
+        url: '/valuations/NOEXIST',
+        method: 'GET',
+      });
+
+      expect(res.statusCode).toBe(404);
+      expect(res.json()).toEqual({
+        message: 'Valuation for VRM NOEXIST not found',
+        statusCode: 404,
+      });
+    });
+
+    it('should return 200 with existing valuation', async () => {
+      const repo = fastify.orm.getRepository(VehicleValuation);
+
+        const valuation = new VehicleValuation();
+        valuation.vrm = "ABC123";
+        valuation.lowestValue = 5000
+        valuation.highestValue = 8000;
+
+        await repo.insert(valuation);
+
+      const res = await fastify.inject({
+        url: '/valuations/ABC123',
+        method: 'GET',
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.json()).toEqual({
+        vrm: 'ABC123',
+        lowestValue: 5000,
+        highestValue: 8000,
+      });
+    });
+  });
   describe('PUT /valuations/', () => {
     it('should return 404 if VRM is missing', async () => {
       const requestBody: VehicleValuationRequest = {
